90bdcc24a0d71ef7c0e62ed18a3c630f66caa54d
Maxim Zaev <max.klint79@gmail.com>
2020-05-15 22:50:19 +0200

    clean up

--- horn.lua
+++ horn.lua
@@ -1,166 +1,286 @@
 require("waveguide")
 
+-- valve patterns
+keys = {                  -6,-5,-4,-3,-2,-1,
+         0,-6,-5,-4,-3,-2,-1, 0,-4,-3,-2,-1,
+         0,-3,-2,-1, 0,-2,-1, 0,-4,-3,-2,-1, 0}
+
+param = {
+-0.013200667274239,
+-0.017510339559265,
+-0.022888981499059,
+-0.029523198334262,
+-0.037736038266239,
+-0.047762424301628,
+-0.059965722025053,
+-0.072371529980717,
+-0.091087702147866,
+-0.11114586491364,
+-0.13416999889321,
+-0.1626324836453,
+-0.19605287123051,
+-0.23787127618823,
+-0.28865529935575,
+-0.34453017492341,
+-0.39876681848583,
+-0.46763376195722,
+-0.54572131719314,
+-0.75191280973749,
+-0.86653169379421,
+-0.98001317964498,
+-1.1159657705301,
+-1.1070956955478,
+-1.259605579122,
+-1.4391258357219,
+-1.6716961396856,
+-1.9020012612799,
+-2.1557265143301,
+-2.4422097153421,
+-2.7626185367762}
+
 Flute = {}
 
-function Flute:new(length)
-	local new = {}	
-	setmetatable(new, self)
-	self.__index = self
+function Flute:new()
+    local new = {}	
+    setmetatable(new, self)
+    self.__index = self
 
-	new.a = 0
-	new.b = 0
+    new.t = 0
 
-	new.wave = Waveguide:new(400)
-	new.wave.length = 44100/233
-	new.valve1 = Waveguide:new(30)
-	new.valve1.length = 23.19
-	new.valve2 = Waveguide:new(20)
-	new.valve2.length = 11.145
-	new.valve3 = Waveguide:new(40)
-	new.valve3.length = 36.028
-	new.filter = 0
-	new.filter2 = 0
-	
-	new.allpass = 0
-	new.allpass2 = 0
-	new.allpass3 = 0
+    new.env = 0
+    new.env2 = 0
+    new.env3 = 0
+    new.keyOn = false
 
-	new.sv = 0
-	new.sx = 0
+    new.x = 0
 
-	new.pv = 0
-	new.px = 0
+    new.out = 0.1
+    new.pout = 0.1
+    new.lastZero = 0
+    new.fdetect = 0
+    new.pdetect = 0
+    new.ptarget = 38
 
-	new.v1 = 0
-	new.v2 = 0
-	new.v3 = 0
+    new.errp = 0
+    new.erri = 0
+    new.errd = 0
+    new.errpp = 0
 
-	new.v1_ = 0
-	new.v2_ = 0
-	new.v3_ = 0
+    new.b = 0
 
-	return new
+    new.wave = Waveguide:new(400)
+    new.wave.length = 44100/233
+    new.valve1 = Waveguide:new(30)
+    new.valve1.length = 23.19
+    new.valve2 = Waveguide:new(20)
+    new.valve2.length = 11.145
+    new.valve3 = Waveguide:new(40)
+    new.valve3.length = 36.028
+    new.filter = 0
+    new.filter2 = 0
+    
+    new.sv = 0
+    new.sx = 0
+
+    new.pv = 0
+    new.px = 0
+
+    new.v1 = 0
+    new.v2 = 0
+    new.v3 = 0
+
+    new.v1_ = 0
+    new.v2_ = 0
+    new.v3_ = 0
+
+    return new
+end
+
+function Flute:noteOn(note)
+    if not note then
+        note = self.ptarget
+    end
+
+    self.ptarget = note
+
+    self:setValvePattern()
+
+    if self.keyOn == false then
+        print("jump")
+        --set valves instantly
+        self.v1_ = self.v1
+        self.v2_ = self.v2
+        self.v3_ = self.v3
+        --reset env only when not legato
+        self.env2 = 0
+        self.env3 = 0
+
+        --quadratic fit
+        local ftarget = math.pow(2,(self.ptarget-49)/12)*440
+        self.b = -8.83e-3 + 6.94e-4*ftarget - 3.94e-6*ftarget*ftarget
+
+        --low notes from array
+        if self.ptarget - 32 <= 5 then
+            self.b = param[self.ptarget - 31]
+        end
+    end
+
+    self.keyOn = true
+    self.erri = 0
+end
+
+function Flute:setValvePattern()
+    local p = keys[self.ptarget - 31] or 0
+
+    if p == 0 then
+        self.v1 = 0
+        self.v2 = 0
+        self.v3 = 0
+    elseif p == -1 then
+        self.v1 = 0
+        self.v2 = 1
+        self.v3 = 0
+    elseif p == -2 then
+        self.v1 = 1
+        self.v2 = 0
+        self.v3 = 0
+    elseif p == -3 then
+        self.v1 = 1
+        self.v2 = 1
+        self.v3 = 0
+    elseif p == -4 then
+        self.v1 = 0
+        self.v2 = 1
+        self.v3 = 1
+    elseif p == -5 then
+        self.v1 = 1
+        self.v2 = 0
+        self.v3 = 1
+    elseif p == -6 then
+        self.v1 = 1
+        self.v2 = 1
+        self.v3 = 1
+    end
+end
+
+function Flute:noteOff(note) 
+    self.keyOn = false
+end
+
+function Flute:setX(x)
+    self.x = x
 end
 
 function Flute:update()
- 	self.px,self.pv = self.sx,self.sv
+    if self.keyOn then
+        self.env = self.env*0.99 + 1*0.01
+    else
+        self.env = self.env*0.99
+    end
 
-	local l,r = self.wave:peek()
-	l,r = -l,-r
+    self.env2 = self.env2 + 40/44100
+    self.env2 = math.min(self.env2, self.x)
 
+    if self.env3 < 1 then
+        self.env3 = self.env3 + 0.5/44100
+    end
 
-	dt = 1/(44100*1)
+    local dt = 1/44100
+    self.t = self.t + dt
 
-	local x = self.sx
-	local y = self.sv
-	local a = 0.05-0.20*env
-	local b = self.b 
+    self.px,self.pv = self.sx,self.sv
 
-	--pitch env onset
-	--b = b*(0.5+0.5*env2)
+    local l,r = self.wave:peek()
+    l,r = -l,-r
 
-	if(keyOn) then
-		--b = b + 0.04*errp + 0.04*erri --+ 0.5*errd
-	end
+    local x = self.sx
+    local y = self.sv
+    local a = 0.05-0.20*self.env
+    local b = self.b
 
-	--error correction PID
-	if env3 > 0.2 then
-		b = b + (0.04*errp + 0.2*erri)
-		b = math.min(b,1.0)
-	end
+    --error correction PID
+    if self.env3 > 0.2 then
+        b = b + (0.04*self.errp + 0.2*self.erri)
+        b = math.min(b,1.0)
+    end
 
-	self.f = b
+    self.f = b
 
-	
+    --vibrato
+    b = b*(1.0 + self.env3*0.1*math.sin(self.t*25 + 0.3*math.sin(self.t*17)))
 
-	--vibrato
-	b = b*(1.0 + env3*0.1*math.sin(t*25 + 0.3*math.sin(t*17)))
-
-	
-	b = b*env --+ (-mouseX_+0.5)*0.5
-	local g = 3500--3500
-
-
-	for i = 1,1 do
-		self.sv = self.sv + (g*g*a + g*g*b*x + g*g*x*x - g*x*y - g*g*x*x*x - g*x*x*y)*dt + love.math.randomNormal(0.1) + 220*l --220*r
-		--self.sv = math.max(math.min(self.sv,44100),-44100)
-		self.sx = self.sx + self.sv*dt 		
-	end
-	
-
-	l = self.sx*0.5 + l * env2*0.6
+    b = b * self.env
+    local g = 3500
 
 
+    for i = 1,1 do
+        self.sv = self.sv + (g*g*a + g*g*b*x + g*g*x*x - g*x*y - g*g*x*x*x - g*x*x*y)*dt + love.math.randomNormal(0.1) + 220*l
+        self.sx = self.sx + self.sv*dt
+    end
 
+    l = self.sx*0.5 + l * self.env2*0.6
 
-	--0.99
-	local a = 1.0--math.exp(-5*mouseY_)
-	self.filter = self.filter*(1-a) + l*a
-	a = 0.1 - 0.09*env2
-	self.filter2 = self.filter2*(1-a) + self.filter*a
-	l = (self.filter - self.filter2)
-	--l = 1.0*clip((self.filter - self.filter2)*1.3)
+    a = 1.0
+    self.filter = self.filter*(1-a) + l*a
+    a = 0.1 - 0.09*self.env2
+    self.filter2 = self.filter2*(1-a) + self.filter*a
+    l = (self.filter - self.filter2)
 
-	--l = 1.0*clip(l*1.5)
+    local spd = 0.002
 
-	--[[local k = 0.8
-	local ap = self.allpass
-	self.allpass = l - k*ap
-	l = k*self.allpass + ap
+    self.v1_ = self.v1_*(1.0-spd) + self.v1*spd
+    self.v2_ = self.v2_*(1.0-spd) + self.v2*spd
+    self.v3_ = self.v3_*(1.0-spd) + self.v3*spd
 
-	--k = 0.5
+    local l1,r1 = self.valve1:peek()
+    local l2,r2 = self.valve2:peek()
+    local l3,r3 = self.valve3:peek()
 
-	ap = self.allpass2
-	self.allpass2 = l - k*ap
-	l = k*self.allpass2 + ap
+    local o1 = self.v1_
+    local o2 = self.v2_
+    local o3 = self.v3_
 
-	--k = 0.5
-	ap = self.allpass3
-	self.allpass3 = l - k*ap
-	l = k*self.allpass3 + ap]]
+    local a = r
+    local b = a*(1.0-o1) + r1*o1
+    local c = b*(1.0-o2) + r2*o2
 
-	local spd = 0.002
+    local d =  r2*(1.0-o3) + l3*o3
+    local e = (r1*(1.0-o3) + l3*o3)*(1.0-o2) + l2*o2
+    local f = ((r*(1.0-o3) + l3*o3)*(1.0-o2) + l2*o2)*(1.0-o1) + l1*o1
 
-	self.v1_ = self.v1_*(1.0-spd) + self.v1*spd
-	self.v2_ = self.v2_*(1.0-spd) + self.v2*spd
-	self.v3_ = self.v3_*(1.0-spd) + self.v3*spd
+    self.valve1:put(e, a)
+    self.valve2:put(d, b)
+    self.valve3:put(r3, c)
+    self.wave:put(f, l)
 
+    self.pout = self.out
+    self.out = clip(l*0.7)
 
-	local l1,r1 = self.valve1:peek()
-	local l2,r2 = self.valve2:peek()
-	local l3,r3 = self.valve3:peek()
+    if self.out < 0.1 and self.pout > 0.10001 then
+        self.fdetect = 1/(self.t - self.lastZero)
+        self.pdetect = 12 * math.log(self.fdetect/440)/math.log(2) + 49
 
-	local o1 = self.v1_
-	local o2 = self.v2_
-	local o3 = self.v3_
+        self.errpp = self.errp
 
-	local a = r
-	local b = a*(1.0-o1) + r1*o1
-	local c = b*(1.0-o2) + r2*o2
+        self.errp = self.errp*0.95 + (self.pdetect - self.ptarget)*0.05
+        self.erri = self.erri + self.errp*0.01
+        self.errd = self.errp - self.errpp
 
-	local d =  r2*(1.0-o3) + l3*o3
-	local e = (r1*(1.0-o3) + l3*o3)*(1.0-o2) + l2*o2
-	local f = ((r*(1.0-o3) + l3*o3)*(1.0-o2) + l2*o2)*(1.0-o1) + l1*o1
+        if self.env3 < 0.2 then
+            self.erri = 0
+        end
+        self.lastZero = self.t
+    end
 
-	
-
-
-	self.valve1:put(e ,a)
-	self.valve2:put(d ,b)
-	self.valve3:put(r3,c)
-  	  self.wave:put(f ,l)
-
-	local out =  1.0*clip(l*0.7)
-
-	return out
+    return self.out
 end
 
 function clip(x)
-	if(x <= -1) then
-		return -2/3
-	elseif(x >= 1) then
-		return 2/3
-	else
-		return x-(x^3)/3
-	end
+    if(x <= -1) then
+        return -2/3
+    elseif(x >= 1) then
+        return 2/3
+    else
+        return x-(x^3)/3
+    end
 end
--- scope.lua
+++ scope.lua
@@ -1,41 +1,39 @@
-require("waveguide")
-require("filter")
-
 Scope = {}
 
-function Scope:new(length)
-	local new = {}	
-	setmetatable(new, self)
-	self.__index = self
 
-	new.input = {}
-	for i = 1,1024 do
-		new.input[i] = 0
-	end
-	new.index = 1
-	new.wait = false
-	new.length = 44100 / 235
+function Scope:new(width, height)
+    local new = {}	
+    setmetatable(new, self)
+    self.__index = self
 
-	return new
+    new.input = {}
+    for i = 1,1024 do
+        new.input[i] = 0
+    end
+    new.index = 1
+    new.wait = false
+    new.length = 44100 / 235
+
+    new.width = width
+    new.height = height/2
+
+    return new
 end
+
 
 function Scope:update(a)
-	--if not self.wait then
-		self.input[self.index] = a
-		self.index = self.index + 1
-		if(self.index > self.length*2) then
-			self.index = 1
-			self.wait = true
-		end
-	--end
- 	
+    self.input[self.index] = a
+    self.index = self.index + 1
+    if(self.index > self.length*3) then
+        self.index = 1
+    end
 end
 
+
 function Scope:draw()
-	love.graphics.setColor(0.5,1.0,0.5)
-	for i=2,self.length*2 do
-		
-		love.graphics.line(i-1,200+self.input[i-1]*100,i,200+self.input[i]*100)
-	end
-	self.wait = false
+    love.graphics.setColor(0.5,1.0,0.5)
+    for i=2, self.length*3 do
+        local xscale = self.width/(self.length*3)
+        love.graphics.line((i-1)*xscale,self.height+self.input[i-1]*self.height/2,i*xscale,self.height+self.input[i]*self.height/2)
+    end
 end
